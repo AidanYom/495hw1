a642bac41a3ca6920f1cc657cf6412d12a64f7a9
AidanYom <aidan@Aidan>
2023-10-11 14:48:05 -0900

    reorganized and commented code

--- learningUtils.py
+++ learningUtils.py
@@ -128,3 +128,86 @@ def updateParams(initialW, initialB, gradW, gradB, learning_rate):
     newB = vSub(initialB, vMultiplyConstant(gradB, learning_rate))
 
     return newW, newB
+
+def printLoss(y_train, A2, iterNum):
+    aggLoss = 0
+    for i in range(len(A2)):
+        aggLoss += loss(y_train[i], A2[i])
+
+    print("iteration:", iterNum, "loss:", aggLoss)
+
+def backPropagation(Z1, A1, Z2, A2, w1, w2, X, Y):
+
+#calculations for the output layer
+    
+    #calculate the aggregate of the gradient of the loss function across all inputs of X
+    #this is equal to the 1/num_samples * SUMMATION(2 * (Prediction - Output))
+    aggGradLoss = [0] * len(A2[0])
+    for i in range(len(A2)):
+        aggGradLoss = vAdd(aggGradLoss, vMultiplyConstant((vSub(A2[i], Y[i])), 2 / len(A2)))
+    
+    #calculate the aggregate of the gradient of the sigmoid function across all inputs X
+    #this is equal to 1/num_samples * SUMMATION(derivative of the sigmoid evaluated at each point in Z2)
+    #Where Z2 is the output of the fully connected linear layer before the activation function
+    aggGradSigmoid = [0] * len(Z2[0])
+    for i in range(len(Z2)):
+        aggGradSigmoid = vAdd(aggGradSigmoid, vMultiplyConstant(sigmoid_layer_derivative(Z2[i]), 1 / len(Z2)))
+
+    #calculate the gradient of the output with respect to the bias
+    #from chain rule, this is the product of the aggregate gradient loss, and the aggregate gradient of the sigmoid
+    #this loop calculates the aggregate weight across all X
+    gradBiases = vMult(aggGradLoss, aggGradSigmoid)
+
+    #calculate the gradient of the output with respect to the weights
+    #from chain rule, this is the gradient of the biases, multiplied by the output of the previous layer
+    #this loop calculates the aggregate weight across all X
+    gradWeights = mMult(gradBiases, vMultiplyConstant(A1[0], 1 / len(A1)))
+    for i in range(1, len(A1)):
+        gradWeights = mAdd(gradWeights, mMult(gradBiases, vMultiplyConstant(A1[i], 1 / len(A1))))
+
+#calculation for the hidden layer
+
+    #calculate the aggregate of the gradient of the sigmoid layer across all inputs X
+    #Z1 is the output of the fully connected linear layer from the hidden layer, before activation function
+    #this follows the same process as above, but with the outputs from the hidden layer
+    aggGradSigmoid1 = [0] * len(Z1[0])
+    for i in range(len(Z1)):
+        aggGradSigmoid1 = vAdd(aggGradSigmoid1, vMultiplyConstant(sigmoid_layer_derivative(Z1[i]), 1 / len(Z1)))
+    
+    #calculate the gradient of the output with respect to the biases of the first hidden layer
+    #this is the product of W2 (the weights of the output layer) transposed, and the 
+    #the gradient of the output with respect to the bias of the output layer, multiplied by the 
+    #previous sigmoid gradient calculated for this stage
+    gradBiases1 = vMult(mMultiplyVector(transpose(w2), gradBiases), aggGradSigmoid1)
+    
+    #this is the gradient of the output with respect to the weights of the first layer
+    #this is the aggregate of the product of the gradient of the output with respect to the first
+    #biases, and the inputs
+    gradWeights1 = mMult(gradBiases1, vMultiplyConstant(X[0], 1 / len(X)))
+    for i in range(1, len(X)):
+        gradWeights1 = mAdd(gradWeights1, mMult(gradBiases1, vMultiplyConstant(X[i], 1 / len(X))))
+
+    print()
+    print(gradWeights)
+    print()
+    print(gradBiases)
+    print()
+    print(gradWeights1)
+    print()
+    print(gradBiases1)
+    print()
+
+    return gradWeights, gradBiases, gradWeights1, gradBiases1
+
+def printAccuracy(test_pred, y_test):
+    predictions = [int(a[0] > .5) for a in test_pred]
+
+    num_correct = 0
+
+    for pred, actual in zip(predictions, y_test):
+        if(pred == actual[0]):
+            num_correct += 1
+
+    accuracy = num_correct / len(predictions) * 100
+
+    print("accuracy:", accuracy, "%")
--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from learningUtils import *
 from random import shuffle
 
-learningRate = 1
+learningRate = .5
 
-numIterations = 20
+numIterations = 1
 
 inputDim = 2
 outputDim = 1
 
-hiddenLayerDim = 10
+hiddenLayerDim = 4
 
-scale = 5
+scale = 1
 
 X = [[0, 0, 0], [1, 0, 1], [0, 1, 1], [1, 1, 0]] * scale
 shuffle(X)
 
-split = int((len(X) * 4) / 5)
+split = int((len(X) * 3) / 4)
 
 X_train = [x[0:2] for x in X[0:split]]
 X_test = [x[0:2] for x in X[split:]]
@@ -25,45 +25,13 @@ y_test = [x[2:] for x in X[split:]]
 # adapted from https://github.com/qobi/ece49595cv/blob/main/two_layer_perceptron.py
 w1, b1, w2, b2 = initialize(inputDim, outputDim, hiddenLayerDim)
 
-for iterNum in range(numIterations):
+for iterNum in range(1, numIterations + 1):
     Z1, A1, Z2, A2 = forward_prop(w1, b1, w2, b2, X_train)
 
-    aggLoss = 0
-    for i in range(len(A2)):
-        aggLoss += loss(y_train[i], A2[i])
+    if iterNum  % 5 == 0:
+        printLoss(y_train, A2, iterNum)
 
-    print("iteration:", iterNum, "loss:", aggLoss)
-
-    aggGradLoss = [0] * len(A2[0])
-
-    for i in range(len(A2)):
-        aggGradLoss = vAdd(aggGradLoss, vMultiplyConstant((vSub(A2[i], y_train[i])), 2 / len(A2)))
-    
-    aggLoss = loss(A2[i], y_train[i])
-    
-    aggGradSigmoid = [0] * len(Z2[0])
-
-    for i in range(len(Z2)):
-        aggGradSigmoid = vAdd(aggGradSigmoid, vMultiplyConstant(sigmoid_layer_derivative(Z2[i]), 1 / len(Z2)))
-
-    gradBiases = vMult(aggGradLoss, aggGradSigmoid)
-
-    gradWeights = mMult(gradBiases, vMultiplyConstant(A1[0], 1 / len(A1)))
-
-    for i in range(1, len(A1)):
-        gradWeights = mAdd(gradWeights, mMult(gradBiases, vMultiplyConstant(A1[i], 1 / len(A1))))
-
-    aggGradSigmoid1 = [0] * len(Z1[0])
-
-    for i in range(len(Z1)):
-        aggGradSigmoid1 = vAdd(aggGradSigmoid1, vMultiplyConstant(sigmoid_layer_derivative(Z1[i]), 1 / len(Z1)))
-    
-    gradBiases1 = vMult(mMultiplyVector(transpose(w2), gradBiases), aggGradSigmoid1)
-    
-    gradWeights1 = mMult(gradBiases1, vMultiplyConstant(X_train[0], 1 / len(X_train)))
-
-    for i in range(1, len(X_train)):
-        gradWeights1 = mAdd(gradWeights1, mMult(gradBiases1, vMultiplyConstant(X_train[i], 1 / len(X_train))))
+    gradWeights, gradBiases, gradWeights1, gradBiases1 = backPropagation(Z1, A1, Z2, A2, w1, w2, X_train, y_train)
 
     w2, b2 = updateParams(w2, b2, gradWeights, gradBiases, learningRate)
     w1, b1 = updateParams(w1, b1, gradWeights1, gradBiases1, learningRate)
@@ -71,14 +39,4 @@ for iterNum in range(numIterations):
 
 Z1, A1, Z2, A2 = forward_prop(w1, b1, w2, b2, X_test)
 
-predictions = [int(a[0] > .5) for a in A2]
-
-num_correct = 0
-
-for pred, actual in zip(predictions, y_test):
-    if(pred == actual[0]):
-        num_correct += 1
-
-accuracy = num_correct / len(predictions) * 100
-
-print("accuracy:", accuracy, "%")
+printAccuracy(A2, y_test)
