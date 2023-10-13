003e95f94be710039d001d11294405dd326732ab
AidanYom <aidan@Aidan>
2023-10-12 20:41:04 -0400

    not working before backs

--- learningUtils.py
+++ learningUtils.py
@@ -129,12 +129,12 @@ def updateParams(initialW, initialB, gradW, gradB, learning_rate):
 
     return newW, newB
 
-def printLoss(y_train, A2, iterNum):
+def printLoss(y_train, A2):
     aggLoss = 0
     for i in range(len(A2)):
         aggLoss += loss(y_train[i], A2[i])
 
-    print("iteration:", iterNum, "loss:", aggLoss)
+    print("Total Squared Loss:", aggLoss)
 
 def backPropagation(Z1, A1, Z2, A2, w1, w2, X, Y):
 
@@ -179,6 +179,14 @@ def backPropagation(Z1, A1, Z2, A2, w1, w2, X, Y):
 
     return gradWeights, gradBiases, gradWeightsNext, gradBiasesNext
 
+def splitInput(input, split, line):
+    X_train = [x[0:line] for x in input[0:split]]
+    X_test = [x[0:line] for x in input[split:]]
+    y_train = [x[line:] for x in input[0:split]]
+    y_test = [x[line:] for x in input[split:]]
+
+    return X_train, X_test, y_train, y_test
+
 
 def printAccuracy(test_pred, y_test):
     predictions = [int(a[0] > .5) for a in test_pred]
@@ -192,3 +200,61 @@ def printAccuracy(test_pred, y_test):
     accuracy = num_correct / len(predictions) * 100
 
     print("accuracy:", accuracy, "%")
+
+def trainAndTest(inputDim, outputDim, hiddenLayerDim, X_train, X_test, y_train, y_test, name, learningRate, numIterations):
+    print("Training and testing of", name,"\n")
+    print("The Hyperparameters are as follows:")
+    print("Number of hidden layers: 1")
+    print("Hidden layer(s) dimension:", hiddenLayerDim)
+    print("Learning Rate:", learningRate)
+    print("Number of epochs: ", numIterations)
+    print("Number of Samples used: ", len(X_train) + len(X_test))
+    print("Training and Test Split: 80/20")
+    print()
+
+
+    w1, b1, w2, b2 = initialize(inputDim, outputDim, hiddenLayerDim)
+
+    for iterNum in range(1, numIterations + 1):
+        Z1, A1, Z2, A2 = forward_prop(w1, b1, w2, b2, X_train)
+
+        netGW2 = w2
+        netGB2 = b2
+        netGW1 = w1
+        netGB1 = b1
+
+        for i in range(len(X_train)):
+            gradWeights2, gradBiases2, gradWeights1, gradBiases1 = backPropagation(Z1[i], A1[i], Z2[i], A2[i], w1, w2, X_train[i], y_train[i])
+            netGW2, netGB2 = updateParams(w2, b2, gradWeights2, gradBiases2, learningRate)
+            netGW1, netGB1 = updateParams(w1, b1, gradWeights1, gradBiases1, learningRate)
+
+        w2 = netGW2
+        b2 = netGB2
+        w1 = netGW1
+        b1 = netGB1
+
+
+    Z1, A1, Z2, A2 = forward_prop(w1, b1, w2, b2, X_test)
+
+    printLoss(y_train, A2)
+    printAccuracy(A2, y_test)
+    print("\n")
+
+#adapted from chat.openai.com
+def generate_two_bit_adder_dataset():
+    dataset = []
+    for a0 in range(2):
+        for b0 in range(2):
+            for c0 in range(2):
+                for a1 in range(2):
+                    for b1 in range(2):
+                        # Calculate the outputs
+                        s0 = (a0 ^ b0) ^ c0
+                        s1 = (a1 ^ b1) ^ (s0 & c0)
+                        c2 = (s0 & c0) | (a1 & b1)
+
+                        # Append the inputs and outputs as a list
+                        dataset.append([a0, b0, c0, a1, b1, s0, s1, c2])
+
+    return dataset
+
--- main.py
+++ main.py
@@ -2,54 +2,39 @@ from learningUtils import *
 from random import shuffle
 
 learningRate = .01
-
 numIterations = 1000
 
-inputDim = 2
-outputDim = 1
+XOR_inputDim = 2
+XOR_outputDim = 1
 
-hiddenLayerDim = 8
+XOR_hiddenLayerDim = 4
 #two layers with a sufficient number of hidden nodes
 #if the input is n, and output is 2^n, it can learn anything
 #this is only for binary inputs
 
 scale = 100
 
-X = [[0, 0, 0], [1, 0, 1], [0, 1, 1], [1, 1, 0]] * scale
-shuffle(X)
+XOR = [[0, 0, 0], [1, 0, 1], [0, 1, 1], [1, 1, 0]] * scale
+shuffle(XOR)
 
-split = int((len(X) * 4) / 5)
+split = int((len(XOR) * 4) / 5)
 
-X_train = [x[0:2] for x in X[0:split]]
-X_test = [x[0:2] for x in X[split:]]
-y_train = [x[2:] for x in X[0:split]]
-y_test = [x[2:] for x in X[split:]]
-
-# adapted from https://github.com/qobi/ece49595cv/blob/main/two_layer_perceptron.py
-w1, b1, w2, b2 = initialize(inputDim, outputDim, hiddenLayerDim)
-
-for iterNum in range(1, numIterations + 1):
-    Z1, A1, Z2, A2 = forward_prop(w1, b1, w2, b2, X_train)
-
-    if iterNum  % 100 == 0:
-        printLoss(y_train, A2, iterNum)
-
-    netGW2 = w2
-    netGB2 = b2
-    netGW1 = w1
-    netGB1 = b1
-
-    for i in range(len(X_train)):
-        gradWeights2, gradBiases2, gradWeights1, gradBiases1 = backPropagation(Z1[i], A1[i], Z2[i], A2[i], w1, w2, X_train[i], y_train[i])
-        netGW2, netGB2 = updateParams(w2, b2, gradWeights2, gradBiases2, learningRate)
-        netGW1, netGB1 = updateParams(w1, b1, gradWeights1, gradBiases1, learningRate)
-
-    w2 = netGW2
-    b2 = netGB2
-    w1 = netGW1
-    b1 = netGB1
+XOR_X_train, XOR_X_test, XOR_y_train, XOR_y_test = splitInput(XOR, split, 2)
 
 
-Z1, A1, Z2, A2 = forward_prop(w1, b1, w2, b2, X_test)
 
-printAccuracy(A2, y_test)
+adder_inputDim = 5
+adder_outputDim = 3
+adder_hiddenLayerDim = 32
+
+adder = generate_two_bit_adder_dataset() * scale
+shuffle(adder)
+adder_X_train, adder_X_test, adder_y_train, adder_y_test = splitInput(adder, split, 5)
+
+# for i in range(1, 4):
+    # for learningRate in [.01, .2, .5]: 
+    #     for numIterations in [10, 100, 1000]:
+
+trainAndTest(XOR_inputDim, XOR_outputDim, XOR_hiddenLayerDim, XOR_X_train, XOR_X_test, XOR_y_train, XOR_y_test, "XOR", learningRate, numIterations)
+
+trainAndTest(adder_inputDim, adder_outputDim, adder_hiddenLayerDim, adder_X_train, adder_X_test, adder_y_train, adder_y_test, "Two Bit Adder", learningRate, numIterations)
